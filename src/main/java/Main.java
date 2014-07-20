57dca87fc5cc82b5b9e25377e6bef3bee1232a63
ibobby <ibobby0386@gmail.com>
2014-07-20 17:46:08 +0400

    The Beginning

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by WhiteMountiens on 20.07.2014.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        Interface anInterface = new Interface();
+    }
+}
